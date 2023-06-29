2d34ed109401bb477079d47ab0935a9e1d624645
smit meshiya <smitmeshiya@gmail.com>
2023-06-29 19:11:24 +0530

    pratice

---
+++ introdution/oprator.js
@@ -0,0 +1,24 @@
+let a=10;
+let b=5;
+let c= 'smit'+" "+'meshiya';
+let d= 15;
+let e=20;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(c);
+
+
+
+if(a < 10 && a < 5){
+    console.log('true.');
+} else{
+    console.log('false.');
+}
+ if(d >= 25 || e < 22){
+    console.log('true');
+ } else {
+    console.log('false');
+ }
---
+++ introdution/smit.js
@@ -0,0 +1,16 @@
+document.getElementById("demo");
+/*document.write('meshiya');
+window.alert('ms');
+console.log(document.getElementById("demo"));*/
+
+let a=5;
+const b=10;
+var c=20;
+sum = a+b+c;
+
+{
+    const b=20;
+    var d=5;
+    console.log(b+d);
+}
+console.log(sum);
